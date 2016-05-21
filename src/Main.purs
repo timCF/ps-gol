c914baae1e0492a2d9908b2a0d4a2943f4e27d05
Strain <llStrainll@gmail.com>
2016-05-22 01:17:47 +0300

    resize canvas

--- src/Main.purs
+++ src/Main.purs
@@ -1,42 +1,40 @@
 module Main where
 
 import Prelude
-
 import Data.Int
 import Data.Maybe
-import Data.Array ((..))
 import Data.List
-import Data.Monoid (mempty)
-import Data.Foldable (fold)
-
 import Control.Monad.Eff
-
 import Graphics.Drawing
-import Graphics.Canvas (getCanvasElementById, getContext2D)
-
 import Control.Monad.Eff.Random
 import Control.Monad.Eff.Console
-
-import DOM.HTML(window)
-import DOM.HTML.Window(innerWidth, innerHeight)
+import Control.Bind (ifM)
+import DOM.HTML (window)
+import DOM.HTML.HTMLCanvasElement (height)
+import DOM.HTML.Window (innerWidth, innerHeight)
 import DOM.RequestAnimationFrame (requestAnimationFrame)
+import Data.Array ((..))
+import Data.Foldable (fold)
+import Graphics.Canvas (getCanvasElementById, getContext2D, setCanvasWidth, setCanvasHeight, getCanvasDimensions)
 
 infixl 0 |>
 (|>) a b = b a
 
-newobj w h = circle (toNumber w) (toNumber h) 10.0 |> filled (fillColor $ fromInt 0xff0000)
+newobj w h = circle w h 10.0 |> filled (fillColor $ fromInt 0xff0000)
 alpha w h = rectangle 0.0 0.0 w h |> filled (fillColor $ rgba 0 0 0 0.1)
 
 loop = requestAnimationFrame do
 	Just canvas <- getCanvasElementById "canvas"
 	ctx <- getContext2D canvas
-	this_width <- window >>= \w -> innerWidth(w)
-	this_heigth <- window >>= \w -> innerHeight(w)
-	w <- randomInt 0 this_width
-	h <- randomInt 0 this_heigth
-	render ctx $ alpha (toNumber this_width) (toNumber this_heigth)
+	{width: this_width, height: this_heigth} <- getCanvasDimensions(canvas)
+	window >>= innerWidth >>= \n -> return (toNumber n) >>= \n -> if (n /= this_width) then setCanvasWidth n canvas else return canvas
+	window >>= innerHeight >>= \n -> return (toNumber n) >>= \n -> if (n /= this_heigth) then setCanvasHeight n canvas else return canvas
+	-- render background and meltdown effect
+	render ctx $ alpha this_width this_heigth
+	w <- random >>= \n -> return $ n * this_width
+	h <- random >>= \n -> return $ n * this_heigth
+	-- render new object
 	render ctx $ newobj w h
-	print [this_width, this_heigth]
 	loop
 
 main = do loop
